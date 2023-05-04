fa77f1855cb927bc5240051f663217181183e109
MTHSKM <105982883+MTHSKM@users.noreply.github.com>
2023-05-04 19:12:35 -0300

    Add files via upload

--- api/models/forms_answers.js
+++ api/models/forms_answers.js
@@ -1,6 +1,6 @@
 'use strict';
 const {
-  Model
+  Model, UniqueConstraintError
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class forms_answers extends Model {
@@ -13,9 +13,17 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
+
   forms_answers.init({
     name: DataTypes.STRING,
-    email: DataTypes.STRING,
+    email: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      unique: true,
+      validate:{
+        is: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/i
+      }
+      },
     cpf: DataTypes.STRING,
     phone: DataTypes.STRING,
     create_at: DataTypes.DATE
